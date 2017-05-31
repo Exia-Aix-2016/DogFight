86751f832f82e41da42884ef59d9e467eab072ab
Hougo13 <hougo13@gmail.com>
2017-05-31 16:09:15 +0200

    finish UserOrder

--- src/Java/jpu2016/dogfight/controller/UserOrder.java
+++ src/Java/jpu2016/dogfight/controller/UserOrder.java
@@ -2,18 +2,20 @@ package jpu2016.dogfight.controller;
 
 public class UserOrder implements IUserOrder {
     private int player;
+    private Order order;
 
     public UserOrder(int player, Order order){
-
+        this.player = player;
+        this.order = order;
     }
 
     @Override
     public Order getOrder(){
-        return Order.DOWN;
+        return this.order;
     }
 
     @Override
     public int getPlayer() {
-        return player;
+        return this.player;
     }
 }

c52f812c90b85d3665669af1300987edc0cfaf2a
NooGat <kinghyde111@gmail.com>
2017-05-31 10:53:05 +0200

    Add .view

--- src/Java/jpu2016/dogfight/view/DogfightView.java
+++ src/Java/jpu2016/dogfight/view/DogfightView.java
@@ -1,4 +1,24 @@
 package jpu2016.dogfight.view;
 
-public class DogfightView {
+import jpu2016.gameframe.GameFrame;
+
+public class DogfightView implements Runnable, IViewSystem {
+    private EventPerformer eventPerformer;
+    private GraphicsBuilder graphicsBuilder;
+    private GameFrame gameFrame;
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void displayMessage(String message) {
+
+    }
+
+    @Override
+    public void closeAll() {
+
+    }
 }
---
+++ src/Java/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,15 @@
+package jpu2016.dogfight.view;
+
+import java.awt.event.KeyEvent;
+
+public class EventPerformer extends IEventPerformer {
+    private IOrderPerformer orderPerformer;
+
+    public EventPerformer(IOderPerformer oderPerformer){}
+
+    public void eventPerformer(KeyEvent KeyCode){}
+
+    private UserOrder keyCodeToUserOrder (int keyCode){}
+
+
+}

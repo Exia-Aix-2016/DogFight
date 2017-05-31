72ce7283eb3ac336ad50e69e2f3887167f2969ad
Hougo13 <hougo13@gmail.com>
2017-05-31 11:04:08 +0200

    set IViewSystem

--- src/Java/jpu2016/dogfight/controller/DogfightController.java
+++ src/Java/jpu2016/dogfight/controller/DogfightController.java
@@ -1,7 +1,10 @@
 package jpu2016.dogfight.controller;
 
+import jpu2016.dogfight.view.IViewSystem;
+
 public class DogfightController implements IOrderPerformer {
     private static int TIME_SLEEP = 30;
+    private IViewSystem viewSystem;
 
     public DogfightController(){
 
@@ -16,8 +19,8 @@ public class DogfightController implements IOrderPerformer {
 
     }
 
-    public void setViewSystem(){
-
+    public void setViewSystem(IViewSystem viewSystem){
+        this.viewSystem = viewSystem;
     }
 
     private void lauchMissile(int player){

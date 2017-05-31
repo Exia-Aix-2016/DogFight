b4be7c3dc27452308d0ea1eda5f9fc7ae862941b
NooGat <kinghyde111@gmail.com>
2017-05-31 17:09:43 +0200

    Finish GameFrame

--- src/Java/jpu2016/gameframe/GamePanel.java
+++ src/Java/jpu2016/gameframe/GamePanel.java
@@ -1,22 +1,31 @@
 package jpu2016.gameframe;
 
+import jpu2016.dogfight.model.IMobile;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
 public class GamePanel extends JPanel implements Observer {
-    public GamePanel(IGraphicsBuilder graphicBuilder){
+    private Observable observable;
+    private IGraphicsBuilder graphicsBuilder;
 
+    public GamePanel(IGraphicsBuilder graphicBuilder){
+        this.graphicsBuilder = graphicBuilder;
     }
 
     @Override
     public void update(Observable observable, Object arg) {
-
+        this.observable = observable;
     }
 
     @Override
     protected void paintComponent(Graphics graphic) {
         super.paintComponent(graphic);
+
+        IMobile mobile = (IMobile) this.observable;
+        this.graphicsBuilder = graphicsBuilder;
     }
+
 }

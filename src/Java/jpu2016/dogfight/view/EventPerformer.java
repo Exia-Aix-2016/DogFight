0be6cb8eebfe6db70ab4098511b8f25420debc87
NooGat <kinghyde111@gmail.com>
2017-05-31 17:36:59 +0200

    Work In Progress

--- src/Java/jpu2016/dogfight/view/EventPerformer.java
+++ src/Java/jpu2016/dogfight/view/EventPerformer.java
@@ -1,6 +1,7 @@
 package jpu2016.dogfight.view;
 
 import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.controller.Order;
 import jpu2016.dogfight.controller.UserOrder;
 import jpu2016.gameframe.IEventPerformer;
 
@@ -9,25 +10,23 @@ import java.awt.event.KeyEvent;
 public abstract class EventPerformer implements IEventPerformer {
     private IOrderPerformer orderPerformer;
 
-    public EventPerformer(){}
-
-    public EventPerformer(IOrderPerformer oderPerformer){}
-
-    public void setOrderPerformer(IOrderPerformer orderPerformer) {
-        this.orderPerformer = orderPerformer;
+    public EventPerformer(IOrderPerformer oderPerformer){
+        this.orderPerformer = oderPerformer;
     }
 
-    public IOrderPerformer getOrderPerformer() {
-        return orderPerformer;
+        public void eventPerformer(KeyEvent KeyCode){
+
     }
-
-    public void eventPerformer(KeyEvent KeyCode){}
-
-
 
     @Deprecated
     private UserOrder keyCodeToUserOrder (int keyCode){
-        return null;
+        switch (keyCode){
+            case 72:
+
+                break;
+        }
+
+        return new UserOrder();
     }
 
     @Override
